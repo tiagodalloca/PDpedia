e7fa527b83fac6c117a98c1e22532f0b0e88ccd5
Tiago Dall'Oca <tdalloca@gmail.com>
2016-09-09 11:11:31 -0300

    imagens - bom progresso! Está funcionando! Argumentos passados pela URL! Salva imagens do sql server!

--- imagens/index.php
+++ imagens/index.php
@@ -9,76 +9,95 @@
   });
 
   $config_file = fopen("config.json", "r");
-	$conteudo = fread($config_file, filesize("config.json"));
+  $conteudo = fread($config_file, filesize("config.json"));
   $config = json_decode($conteudo, true);
   fclose($config_file);
 
   $infos = getURLArgs($config);
   lidarComRequest(
     $infos["metodo"],
-    $infos["URLargs"],
     $infos["db"],
     $config
   );
 
   function getURLArgs($dbconfig){
-    if (isset($_SERVER['PATH_INFO']))
-    {
-      $db = sqlsrv_connect("regulus", $dbconfig);
-      $metodo = strtoupper($_SERVER['REQUEST_METHOD']);
-      $URLargs = explode("---", trim($_SERVER['PATH_INFO'], "---"));
-      return array(
-        "metodo"    => $metodo,
-        "URLargs"  => $URLargs,
-        "db"        => $db
-      );
-    }
+    $metodo = strtoupper($_SERVER['REQUEST_METHOD']);
+    $db = sqlsrv_connect("regulus", $dbconfig);
 
-		die("0");
+    return array(
+      "metodo"    => $metodo,
+      "db"        => $db
+    );
   }
 
-  function lidarComRequest($metodo, $URLargs, $db){
-		if(!$db){
-			die("Não se conectou com o db");
-		}
+  function lidarComRequest($metodo, $db){
+    if(!$db){
+      die("Não se conectou com o db");
+    }
 
     switch ($metodo) {
       case 'GET':
-        $nome = $URLargs[0];
+        $nome = $_GET["nome"];
         $cmd = sqlsrv_prepare($db, "SELECT bytes FROM Imagem WHERE nome = ?", array( &$nome));
-				if(!$cmd)
+
+        if(!$cmd)
           die("0");
-				$imagemBytes = sqlsrv_execute($cmd);
-				if ($imagemBytes != false){
-					$image = imagecreatefromstring($imagemBytes);
-					header('Content-Type: image/jpeg');
-		      imagejpeg($image);
-		      imagedestroy($image);
-				}
-      	else
-        	die("0");
+
+        if (sqlsrv_execute($cmd) != false){
+          $ret = sqlsrv_fetch_array($cmd, SQLSRV_FETCH_ASSOC);
+          $bytes = base64_decode($ret["bytes"]);
+          if ($bytes != ""){
+            $image = imagecreatefromstring($bytes);
+            header('Content-Type: image/jpeg');
+            imagejpeg($image);
+            imagedestroy($image);
+          }
+        }
+        if( ($errors = sqlsrv_errors() ) != null){
+          foreach( $errors as $error ) {
+              echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+              echo "code: ".$error[ 'code']."<br />";
+              echo "message: ".$error[ 'message']."<br />";
+          }
+        }
         break;
 
       case 'POST':
-				$nome = $URLargs[0];
-        $bytes = getImageData($URLargs[1]);
-        $cmd = sqlsrv_prepare($db, "INSERT INTO Imagem VALUES(?, ?, ?)", array( &$nome, &$bytes, &$URLargs[1]));
+        $nome = $_GET["nome"];
+        $bytes = getImageData($_GET["url"]);
+        $bytes = base64_encode($bytes);
+        $cmd = sqlsrv_prepare($db, "INSERT INTO Imagem VALUES(?, ?, ?)", array(&$nome, &$bytes, &$_GET["url"]));
         if (sqlsrv_execute($cmd))
-        	echo "1";
-      	else
-					die("0");
+          echo "1";
+        else{
+          if( ($errors = sqlsrv_errors() ) != null)
+            foreach( $errors as $error ) {
+                echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+                echo "code: ".$error[ 'code']."<br />";
+                echo "message: ".$error[ 'message']."<br />";
+            }
+        }
+
         break;
     }
   }
 
   function getImageData($url){
-    $fp = fopen("$sFilePath", 'rb') or die("404! Deu ruim!");
-    $buf = "";
-    while(!feof($fp)){
-        $buf .= fgets($fp, 4096);
-    }
-    fclose($fp);
-    return $buf; //retorna False se deu ruim, se não o conteúdo daquela imagem
+    // $fp = fopen($url, 'rb') or die("404! Deu ruim!");
+    // $buf = "";
+    // while(!feof($fp)){
+    //     $buf .= fgets($fp, 4096);
+    // }
+    // fclose($fp);
+    // return $buf; //retorna False se deu ruim, se não o conteúdo daquela imagem
+    //
+    $process = curl_init($url);
+    curl_setopt($process, CURLOPT_HEADER, 0);
+    curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($process, CURLOPT_BINARYTRANSFER, 1);
+    $raw = curl_exec($process);
+    curl_close($process);
+    return $raw;
   }
 
 ?>
